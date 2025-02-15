19e7ae5b3db7b5548fb04fe180622d746b29e3ba
AlexMan123456 <alex.man03888@gmail.com>
2025-02-15 13:33:37 +0000

    Added email property to users

--- database/test-data/users.js
+++ database/test-data/users.js
@@ -2,21 +2,25 @@ module.exports = [
     {
         username: "AlexTheMan",
         global_name: "Alex The Man",
+        email: "alex.man03888@gmail.com",
         is_artist: true,
     },
     {
         username: "AlexGB231",
         global_name: "AlexGB231",
+        email: "alexglitchboy231@gmail.com",
         is_artist: true
     },
     {
         username: "Kevin_SynthV",
         global_name: "Kevin",
+        email: "captain-kevin@thefarisland.com",
         is_artist: true
     },
     {
         global_name: "Badstagram",
         username: "Badstagram",
+        email: "test@test.com",
         is_artist: false
     }
 ]
