6164f6c43447899818a26700c37d580fdc64b3b1
AlexMan123456 <alex.man03888@gmail.com>
2025-02-21 01:10:13 +0000

    A few fixes with the seeding

--- prisma/dev-data/users.js
+++ prisma/dev-data/users.js
@@ -1,5 +1,6 @@
 module.exports = [
     {
+        user_id: "5pLK2GyqhFhmRczNz6i16JrtjKH2",
         username: "AlexTheMan",
         artist_name: "Alex The Man",
         email: "alextheman231231@gmail.com"
