63f8ffdf4cd6c2fa1edf15a49fc42a6501ce8f0d
AlexMan123456 <alex.man03888@gmail.com>
2025-02-22 23:05:32 +0000

    Added date of birth and member since to dev data

--- prisma/dev-data/users.js
+++ prisma/dev-data/users.js
@@ -3,6 +3,8 @@ module.exports = [
         user_id: "5pLK2GyqhFhmRczNz6i16JrtjKH2",
         username: "AlexTheMan",
         artist_name: "Alex The Man",
-        email: "alextheman231231@gmail.com"
+        email: "alextheman231231@gmail.com",
+        date_of_birth: new Date("2003-07-16T00:00:00Z"),
+        member_since: new Date("2024-02-15T00:00:00+00:00")
     }
 ]
