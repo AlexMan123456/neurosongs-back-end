de488966588e5e6cceacf7e86467921d29aa0c14
AlexMan123456 <alex.man03888@gmail.com>
2025-02-24 02:13:36 +0000

    Added some more comment test data

--- prisma/test-data/comments.js
+++ prisma/test-data/comments.js
@@ -22,5 +22,23 @@ module.exports = [
         song_id: 7,
         body: "This is a pretty cool song!",
         rating: 7.5
+    },
+    {
+        user_id: "1",
+        album_id: 2,
+        body: "I worked very hard on this album, so I hope everyone enjoys it!",
+        rating: 9
+    },
+    {
+        user_id: "3",
+        album_id: 3,
+        body: "This is the best album, by the way, and I'm totally not biased because it's my own!",
+        rating: 10
+    },
+    {
+        user_id: "4",
+        album_id: "4",
+        body: "This is very cringe",
+        rating: 1
     }
 ]
